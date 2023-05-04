6f38d9ca8d153a8583ec41b2b53ad28042e2cf3e
iwaniwset <jumatkliwon23@gmail.com>
2023-05-04 16:00:44 +0700

    done for adding about us

--- src/components/About.js
+++ src/components/About.js
@@ -5,18 +5,27 @@ export default function About() {
   return (
     <div className="relative">
       <div className="flex z-10  flex-col justify-center items-center px-3 py-4">
-        <h1 className="text-2xl text-sky-950 mb-2  text-center border-sky-900 ">
-          Ayo Gabung Awali Karirmu bersama Eli Crew Cilacap!!!
+        <div className="flex items-center gap-4 pt-2 xl:pb-5">
+          <hr className="w-14 mb-3 h-2 bg-textSecondary rounded-md" />
+
+          <h2 className="text-3xl pb-3 sm:pb-6 lg:text-4xl lg:pb-3 font-semibold text-textPrimary">
+            Tentang Kami
+          </h2>
+          <hr className="w-14 mb-3 h-2 bg-textSecondary rounded-md" />
+        </div>
+        <h1 className="text-3xl font-bold md:text-4xl xl:text-5xl text-primary mb-1 xl:mb-3  text-center border-sky-900 ">
+          Ayo Gabung Bersama
         </h1>
-        <hr className="w-14 h-2 bg-primary rounded-md" />
-        <p className="text-sky-700 text-center tracking-wide">
-          Dengan metode Khusus kami yang efektif dan terintegrasi, kami pandu
-          anda untuk dapat{" "}
-          <span className=" text-sky-900">
-            {" "}
-            bekerja di hotel / kapal pesiar Asia, Eropa & Amerika{" "}
-          </span>
-          dengan cepat mudah dan dengan biaya super hemat
+        <h1 className="text-5xl md:text-7xl xl:text-8xl text-primary font-bold mb-3">
+          Eli Crew Cilacap
+        </h1>
+        <hr className="w-20 mb-5 h-2 bg-primary rounded-md" />
+        <p className="text-slate-500 font-light text-2xl sm:px-5 text-center sm:text-3xl leading-normal sm:leading-relaxed xl:text-4xl xl:leading-relaxed">
+          Eli Crew Cilacap merupakan lembaga pendidikan Profesional Di bidang
+          Training Center Kapal Pesiar, Perhotelan dan Bahasa Inggris yang
+          terletak di pusat kota Cilacap. Dengan adanya Eli diharapkan dapat
+          memenuhi permintaan mitra kerja Eli Crew Cilacap dari dunia perhotelan
+          dan Kapal Pesiar
         </p>
       </div>
       <div className="flex justify-center z-30 py-3  items-center gap-1 w-full bg-sky-900">
