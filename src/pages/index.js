69b6374dcce8b13d61ba8ed61cf95673f61a9e2b
iwaniwset <jumatkliwon23@gmail.com>
2023-05-24 06:46:30 +0700

    try to fix gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
         <Elicrew />
         <About />
         <Pencapaian />
-        <Gallery />
+        {/* <Gallery /> */}
         <Closing />
         <Program />
         <Maps />
